4014d3855786f65ef8069a74ca91300f9d8f2b36
Fabio <fabiolaguna.94@gmail.com>
2020-08-31 16:40:15 -0300

    Ejercicio 3

--- TP 2/index.js
+++ TP 2/index.js
@@ -1,11 +1,31 @@
-window.onload = () => execute();
+/*function insertRow(){
+    const table = document.getElementsByClassName("myTable");
+    const row = document.createElement("tr");
+    const cell = document.createElement("td");
+    const text = document.createTextNode("new row");
+    cell.appendChild(text);
+    row.appendChild(cell);
+    table[0].appendChild(row);
+}
 
-/*function execute(){
-    const text = document.getElementsByClassName("test");
-    text[0].appendChild(document.createTextNode("Hello world"));
+function deleteRow(){
+    const table = document.getElementsByClassName("myTable");
+    const lastRow = table[0].lastChild;
+    table[0].removeChild(lastRow);
 }*/
 
-function execute(){
-    const text = document.querySelector(".test");
-    text.appendChild(document.createTextNode("Hello world"));
+function insertRow(){
+    const table = document.querySelector(".myTable");
+    const row = document.createElement("tr");
+    const cell = document.createElement("td");
+    const text = document.createTextNode("new row");
+    cell.appendChild(text);
+    row.appendChild(cell);
+    table.appendChild(row);
+}
+
+function deleteRow(){
+    const table = document.querySelector(".myTable");
+    const lastRow = table.lastChild;
+    table.removeChild(lastRow);
 }
